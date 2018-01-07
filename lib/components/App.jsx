239d9431dbd6e2fcd439da1cc7f167e834926ede
kuritka <kuritka@gmail.com>
2018-01-07 23:54:50 +0100

    add components

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DataApi from '../dataApi';
+import ArticleList from './ArticleList'
+import {data} from '../__tests__/testData';
+
+
+const api = new DataApi(data);
 
 export default class App extends React.Component {
-  state = {
-    answer: 42
-  };
-  asyncFunc = () => { return Promise.resolve(37)};
-  async componentDidMount(){
-    this.setState({answer: await this.asyncFunc()});
-  }
+  constructor(){
+    super();
+    this.state = {
+         articles: api.getArticles(),
+         authors: api.getAuthors()
+      };
+   }
+  // asyncFunc = () => { return Promise.resolve(37)};
+  // async componentDidMount(){
+  //   this.setState({answer: await this.asyncFunc()});
+  // }
   render() {
     return (
      <div style={{textAlign: 'center'}}>
-        <h1>Hello World !! -- {this.state.answer}</h1>
+        <h1>wwws</h1>
+        <ArticleList 
+          articles={this.state.articles}
+          authors={this.state.authors}
+        />
       </div>);
   }
 }
+
+
+// export default class App extends React.Component {
+//   state = {
+//     answer: 42
+//   };
+//   asyncFunc = () => { return Promise.resolve(37)};
+//   async componentDidMount(){
+//     this.setState({answer: await this.asyncFunc()});
+//   }
+//   render() {
+//     return (
+//      <div style={{textAlign: 'center'}}>
+//         <h1>Hello World !! -- {this.state.answer}</h1>
+//       </div>);
+//   }
+// }
